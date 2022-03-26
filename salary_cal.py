b710d541fa178de75fc4897f757f1d12eb439375
afrar <57473343+hanifaafrar@users.noreply.github.com>
2022-03-26 13:12:14 +0530

    input

--- salary_cal.py
+++ salary_cal.py
@@ -1,2 +1,8 @@
 print("""***Hi, Welcome!***
         ***The program starts***""")
+name = input('Enter Name: \n')
+hour = input('Enter Hours: \n')
+salary = input ('Enter Salary: \n')
+
+houri = int(hour)
+salaryi = int(salary)
