e1c917a83f5f941f46b937895b0556559b6ff016
afrar <57473343+hanifaafrar@users.noreply.github.com>
2022-03-28 12:46:49 +0530

    added function to the program

--- salary_cal.py
+++ salary_cal.py
@@ -1,9 +1,6 @@
 print("""***Hi, Welcome!***
         ***The program starts***""")
 
-def cal(rate, time):
-    print('Your worked salary and hours are:', rate , time)
-
 name = input('Enter Name: \n')
 try:
     hour = int(input('Enter Hours: \n'))
@@ -13,19 +10,17 @@ except:
     print('Hi', name, 'Your entered value is wrong')
     print('&&&& please enter a numeric value &&&&')
 
+def cal(time, rate):
+    print('Your worked salary and hours are:', rate , time)
+    if time > 40:
+        print(name, 'Worked Overtime!')
+        netsalary = rate * time
+        ot = (time - 40) * (rate * 1.5)
+        fullsalary = netsalary + ot
+    else:
+        print(name, "worked Regulartime!")
+        fullsalary = rate * time
+    return print('Total Salary is', fullsalary, ' USD')
+
+
 cal(salary, hour)
-
-if hour > 40:
-    print(name,'Worked Overtime!')
-    netsalary = salary * hour
-    ot = (hour - 40) * (salary * 1.5)
-    fullsalary = netsalary + ot
-else:
-    print(name, "worked Regulartime!")
-    fullsalary = salary * hour
-
-
-
-#Adding the market currency value
-rs = 'USD'
-print('Total Salary is', fullsalary,rs)
