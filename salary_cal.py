63d35767f6f592c44ebc2ea7beaad4f2db3b1b95
afrar <57473343+hanifaafrar@users.noreply.github.com>
2022-03-29 19:43:27 +0530

    used the while loop for the program

--- salary_cal.py
+++ salary_cal.py
@@ -1,26 +1,32 @@
 print("""***Hi, Welcome!***
         ***The program starts***""")
 
-name = input('Enter Name: \n')
-try:
-    hour = int(input('Enter Hours: \n'))
-    salary = int(input('Enter Salary: \n'))
+while True:
+    name = input('Enter Name: \n')
+    try:
+        hour = int(input('Enter Hours: \n'))
+        salary = int(input('Enter Salary: \n'))
 
-except:
-    print('Hi', name, 'Your entered value is wrong')
-    print('&&&& please enter a numeric value &&&&')
-
-def cal(time, rate):
-    print('Your worked salary and hours are:', rate , time)
-    if time > 40:
-        print(name, 'Worked Overtime!')
-        netsalary = rate * time
-        ot = (time - 40) * (rate * 1.5)
-        fullsalary = netsalary + ot
-    else:
-        print(name, "worked Regulartime!")
-        fullsalary = rate * time
-    return print('Total Salary is', fullsalary, ' USD')
+    except:
+        print('Hi', name, 'Your entered value is wrong')
+        print('&&&& please enter a numeric value &&&&')
 
 
-cal(salary, hour)
+    if hour >= 100:
+       break
+
+    def cal(time, rate):
+        print('Your worked salary and hours are:', rate , time)
+        if time > 40:
+            print(name, 'Worked Overtime!')
+            netsalary = rate * time
+            ot = (time - 40) * (rate * 1.5)
+            fullsalary = netsalary + ot
+        else:
+            print(name, "worked Regulartime!")
+            fullsalary = rate * time
+        return print('Total Salary is', fullsalary, ' USD')
+
+
+    cal(salary, hour)
+print('**plz enter valid hours**')
